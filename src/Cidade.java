c4435729dd1b658f06fa5997c3454645f00725bb
Juscelino Fernandes da Costa Junior <juscostajr@gmail.com>
2024-08-13 21:59:19 -0300

    Aula 3 - Encapsulamento

--- src/Cidade.java
+++ src/Cidade.java
@@ -1,6 +1,31 @@
+//Encapsular a classse Cidade,
+//implementar construtor e métodos getter
 public class Cidade {
-    public String nome;
-    public String cep;
-    public Integer populacao;
-    public Estado estado;
+    private String nome;
+    private String cep;
+    private Integer populacao;
+    private Estado estado;
+
+    public Cidade(String nome, String cep, Integer populacao, Estado estado) {
+        this.nome = nome;
+        this.cep = cep;
+        this.populacao = populacao;
+        this.estado = estado;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getCep(){
+        return this.cep;
+    }
+
+    public Integer getPopulacao(){
+        return this.populacao;
+    }
+
+    public Estado getEstado(){
+        return this.estado;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        Carro carro1 = new Carro();
 
-        System.out.println("Informe o modelo do carro");
-        carro1.modelo = sc.next();
-        System.out.println("Informe o ano do carro");
-        carro1.ano = sc.nextInt();
-        System.out.println("Informe a marca do carro");
-        carro1.marca = sc.next();
-        System.out.println("Informe a cor do carro");
-        carro1.cor = sc.next();System.out.println("Modelo: " + carro1.modelo);
-        System.out.println("Ano: " + carro1.ano);
-        System.out.println("Marca: " + carro1.marca);
-        System.out.println("Cor: " + carro1.cor);
+        Carro carro = new Carro(
+                1995,
+                "Monza",
+                "Branco",
+                "Chevrolet"
+        );
 
-        String isentoIpva = carro1.ehIsentoIPVA(2024) ? "é isento" : "não isento";
-        System.out.println("O " + carro1.modelo + " " + isentoIpva + " do IPVA");
+        Carro carro2 = new Carro(
+                2012,
+                "206",
+                "Vermelho",
+                "Peugeot"
+        );
+        System.out.println(carro2.getModelo());
+        System.out.println(carro2.getIsentoIPVA());
+        System.out.println(carro2.getMarca());
 
-        Loja loja = new Loja();
-        Cidade cidade = new Cidade();
-        cidade.nome = "Campo Mourão";
-        cidade.estado = Estado.PR;
-        loja.nome = "Auto Imports";
-        loja.cidade = cidade;
-
-        List<Carro> carros = new ArrayList<>();
-        carros.add(carro1);
-
-        loja.estoque = carros;
 
     }
 }
